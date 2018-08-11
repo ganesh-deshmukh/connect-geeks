09d9c88415103c825987e4fc3a8057a268afc778
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-08-11 13:17:09 +0530

    connected to database successfully

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// configure database
+const db = require("./config/keys").mongoURI;
+
+// connect to database
+// Then returns promise
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch(err => {
+    console.log("Error occured.\n" + err);
+  });
+
 app.get("/", (req, res) => res.send("app.get is working"));
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server is running at port no `{$port}`"));
+app.listen(port, () => console.log(`Server is running at port no ${port}`));
