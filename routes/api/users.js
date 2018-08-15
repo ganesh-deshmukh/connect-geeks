76408e0e78c3395b6c1ab1d11d673a896597bf34
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-08-15 11:59:43 +0530

    added route to register

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,9 +3,20 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User Model to check existing email is used for registration or not?
+const User = require("../../models/User");
+
 // @route       GET request to api/users/test
 // @description Tests users route
 // @access      Public, without login
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+//
+// @route       GET request to api/users/register
+// @description new registration of user.
+// @access      Public, without login first register
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email });
+});
 
 module.exports = router;
