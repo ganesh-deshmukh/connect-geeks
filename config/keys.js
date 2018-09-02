1f942734979b38a18563607ad87c69519f9c1f85
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-09-02 11:43:14 +0530

    added directory /validation and files 'is-empty.js', 'login.js','register.js'

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // mongoURI: "mongodb://gdgeek:gd%40123@ds225382.mlab.com:25382/findgeeks",
+  mongoURI: "mongodb://gdgeek:gd%40123@ds225382.mlab.com:25382/findgeeks",
 
-  mongoURI: "mongodb://localhost:27017/findgeeks",
+  // mongoURI: "mongodb://localhost:27017/findgeeks",
 
   secretOrKey: "secretForJWT" // jwt token
 };
