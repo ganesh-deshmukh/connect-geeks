d78fddca87d8372c6feb060129258220c2b27585
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-11 20:16:14 +0530

    added 'experience-validator' file for exp-details validating fields.

---
+++ validation/experience.js
@@ -0,0 +1,26 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : "";
+  data.company = !isEmpty(data.company) ? data.company : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = "Your Job-title field is required";
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = "Your Job-company field is required";
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "Your Job-Start-Date field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
