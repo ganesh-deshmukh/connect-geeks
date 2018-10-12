2b05a16be932b32cf406a4034238b43f66cc3ea9
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-12 15:46:49 +0530

    created route as 'api/profile/posts'

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,32 @@
 
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// bring Post model Schema
+const Post = require("../../models/Post");
 
 // @route       GET request to api/posts/test
 // @description Tests posts route
 // @access      Public, without login
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 module.exports = router;
+
+// @route       GET request to api/posts
+// @description Create Posts
+// @access      Private, with login
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.user.text,
+      name: req.user.name,
+      avatar: req.user.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
