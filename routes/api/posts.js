4b7a5212e5a9d9fcba108a41282c24ed86b9f652
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-12 18:46:40 +0530

    added route to delete post by its owner after validating owner

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,9 @@ const passport = require("passport");
 // bring Post model Schema
 const Post = require("../../models/Post");
 
+// bring Profile model Schema to validate post's owner
+const Profile = require("../../models/Profile");
+
 // validate post-data input
 const validatePostInput = require("../../validation/post");
 
@@ -23,7 +26,9 @@ router.get("/", (req, res) => {
   Post.find()
     .sort({ date: -1 })
     .then(posts => res.json(posts))
-    .catch(err => res.status(404));
+    .catch(err =>
+      res.status(404).json({ nopostfound: "Sorry, No any post found" })
+    );
 });
 
 // @route       GET request to api/posts/:id
@@ -32,7 +37,9 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
     .then(post => res.json(post))
-    .catch(err => res.status(404));
+    .catch(err =>
+      res.status(404).json({ nopostfound: "Sorry, No post found with this ID" })
+    );
 });
 
 // @route       POST request to api/posts
@@ -57,6 +64,34 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route       DELETE request to api/posts/:id
+// @description delete post of specific user
+// @access      Private post, need to login
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id).then(post => {
+          // check for owner of post or not
+          if (post.user.toString() !== req.user.id) {
+            // means not same user who is owner of post
+            return res
+              .status(401)
+              .json({ notautorized: "User not Authorized" });
+          }
+
+          // User = Owner of post, then delete that post
+          post.remove().then(() => res.json({ success: true }));
+        });
+      })
+      .catch(err =>
+        res.status(404).json({ postnotfound: "No Post found to delete" })
+      );
   }
 );
 
