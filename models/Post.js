00e1c2bd6837eacd13fdeeb6e88d271b3452d39d
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-12 14:24:41 +0530

    completed model for posts with details of post as fields in db

--- models/Post.js
+++ models/Post.js
@@ -10,7 +10,59 @@ const PostSchema = new Schema({
   text: {
     type: String,
     required: true
-  }
+  },
 
   // if users wants to delete their account, their post should not be deleted, as post can be imp.
+
+  // automatically each post will show name and gravator of user
+  name: {
+    type: String
+  },
+  avator: {
+    type: String
+  },
+  likes: {
+    // likes is not just a number, we should show who liked post.
+    likes: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "users"
+        }
+      }
+    ],
+
+    comments: [
+      {
+        // commments will have details of sender or name+avator of Commentator.
+
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "users"
+        },
+        text: {
+          type: String,
+          required: true
+        },
+        name: {
+          type: String
+        },
+        avator: {
+          type: String
+        },
+        date: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ],
+
+    // post also have date of posting
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  }
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);
