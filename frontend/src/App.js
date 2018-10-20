eefba9192e8ed17eef57b46000beb78f34653e04
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-20 21:11:25 +0530

    added token-based login of users and storing token locally & validating.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+
+import { setCurrentUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -9,6 +13,19 @@ import Login from "./components/auth/Login";
 import store from "./store";
 
 import "./App.css";
+
+// check if token is stored in localstorage, if user has logged in.
+
+if (localStorage.jwtToken) {
+  // if token exitst, then set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // now decode token and get user-info, expiry-date of token
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  // set user and user is now authenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- frontend/src/utils/setAuthToken.js
+++ frontend/src/utils/setAuthToken.js
@@ -1,20 +1,3 @@
-// import axios from "axios";
-
-// // attach token with each request in header
-
-// const setAuthToken = token => {
-//   if (token) {
-//     // then apply that token to each request, if not then you are not autorized and get token first.
-
-//     axios.defaults.headers.common["Authorization"] = token;
-//   } else {
-//     // no token
-//     //  then delete auth head
-//     delete axios.defaults.headers.common["Authorization"];
-//   }
-// };
-
-// export default setAuthToken;
 import axios from "axios";
 
 const setAuthToken = token => {
