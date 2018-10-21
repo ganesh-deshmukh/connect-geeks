c79c39ba3aa382399a46b61de7f7e98d482c135b
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-21 19:49:59 +0530

    adding Text-field components in create-profile.js

--- frontend/src/components/common/create-profile/CreateProfile.js
+++ frontend/src/components/common/create-profile/CreateProfile.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import TextFieldGroup from "../../common/TextFieldGroup";
+import TextAreaFieldGroup from "../../common/TextAreaFieldGroup";
+import InputGroup from "../../common/InputGroup";
+import SelectListGroup from "../../common/SelectListGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
