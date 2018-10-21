1951047d3596714ab1655f1279508bf7250c763f
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-21 13:44:01 +0530

    added PrivateRoute.js for Protected-Routes access authorization

---
+++ frontend/src/components/common/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      // check in ternary-singleLineOperator, if true then logged in
+      // then render Component and pass props
+      // else redirect it to '/login'.
+
+      auth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
