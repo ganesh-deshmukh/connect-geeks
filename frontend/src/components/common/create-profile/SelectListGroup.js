8c963fb88f178742cc441569feca5ed7156919d5
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-21 19:07:36 +0530

    choosing options  from select-list using array.map() in select-list-grp

--- frontend/src/components/common/create-profile/SelectListGroup.js
+++ frontend/src/components/common/create-profile/SelectListGroup.js
@@ -2,22 +2,29 @@ import React from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-return (
-  <div className="form-group">
-    <select
-      className={classnames("form-control form-control-lg", {
-        "is-invalid": error
-      })}
-      name={name}
-      value={value}
-      onChange={onChange}
-    >
-      {selectOptions}
-    </select>
-    {info && <small className="form-text text-muted">{info}</small>}
-    {error && <div className="invalid-feedback">{error}</div>}
-  </div>
-);
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
+  const selectOptions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  ));
+  return (
+    <div className="form-group">
+      <select
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        name={name}
+        value={value}
+        onChange={onChange}
+      >
+        {selectOptions}
+      </select>
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
 
 SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
