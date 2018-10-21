76425866cb1b981cd91d51b26435697ab189693d
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-21 18:50:20 +0530

    added SelectListGroup -Component, to select options in profile menu.

---
+++ frontend/src/components/common/create-profile/SelectListGroup.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+return (
+  <div className="form-group">
+    <select
+      className={classnames("form-control form-control-lg", {
+        "is-invalid": error
+      })}
+      name={name}
+      value={value}
+      onChange={onChange}
+    >
+      {selectOptions}
+    </select>
+    {info && <small className="form-text text-muted">{info}</small>}
+    {error && <div className="invalid-feedback">{error}</div>}
+  </div>
+);
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
+export default SelectListGroup;
