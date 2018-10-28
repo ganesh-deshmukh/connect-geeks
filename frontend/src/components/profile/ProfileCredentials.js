c192f93d80f7e8283134f2223d225389a7b494fd
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-28 17:31:34 +0530

    added github username and showing 5 repos, under profile-section.

--- frontend/src/components/profile/ProfileCredentials.js
+++ frontend/src/components/profile/ProfileCredentials.js
@@ -6,7 +6,7 @@ class ProfileCreds extends Component {
     const { experience, education } = this.props;
 
     const expItems = experience.map(exp => (
-      <li key={exp._id} className="list-group-item">
+      <li key={exp._id} className="list-group-item bg-light">
         <h4>{exp.company}</h4>
         <p>
           <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
@@ -37,7 +37,7 @@ class ProfileCreds extends Component {
     ));
 
     const eduItems = education.map(edu => (
-      <li key={edu._id} className="list-group-item">
+      <li key={edu._id} className="list-group-item bg-light">
         <h4>{edu.school}</h4>
         <p>
           <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
@@ -64,7 +64,7 @@ class ProfileCreds extends Component {
     ));
     return (
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-6 ">
           <h3 className="text-center text-info">Education</h3>
           {eduItems.length > 0 ? (
             <ul className="list-group">{eduItems}</ul>
