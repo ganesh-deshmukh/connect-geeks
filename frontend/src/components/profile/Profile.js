2fcee5a294947225578de38fca578c449e100bd5
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2018-10-28 16:51:51 +0530

    added  profile-credentials, education and experience in profile route

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import ProfileHeader from "./ProfileHeader";
 import ProfileAbout from "./ProfileAbout";
-import ProfileCreds from "./ProfileCreds";
+import ProfileCreds from "./ProfileCredentials";
 import ProfileGithub from "./ProfileGithub";
 import Spinner from "../common/Spinner";
 import { getProfileByHandle } from "../../actions/profileActions";
