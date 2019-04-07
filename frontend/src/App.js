c05e6c023bc8f1c34dec162d83d523df25f3fed4
Ganesh Deshmukh <ganeshdeshmukh1612@gmail.com>
2019-04-07 17:12:16 +0530

    added user's post feature with comments' feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 import PrivateRoute from "./components/common/PrivateRoute";
 import NotFound from "./components/not-found/NotFound";
--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostForm from "./PostForm";
+import PostFeed from "./PostFeed";
 import Spinner from "../common/Spinner";
 import { getPosts } from "../../actions/postActions";
 
@@ -11,12 +12,22 @@ class Posts extends Component {
   }
 
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
+
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
